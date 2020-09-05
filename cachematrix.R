531c0afbc67379cb02b3a5f10fbda5fbffd01b2a
yesidrour <quortom@hotmail.com>
2020-09-05 14:22:18 -0500

    This is the fisrt commit. It contains the two functions for the assigment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions will cache and return the inverse of a matrix
 
-## Write a short comment describing this function
+
+## This is the first function. It will create a matrix object
+## than can cache its inverse
+##  1. set the value of the matrix
+##  2. get the value of the matrix
+##  3. set the value of the inverse matrix
+##  4. get the value of the inverse matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 
 }
 
 
-## Write a short comment describing this function
+## The second function calculates the inverse of the matrix created by the fisrt function
+## This function will return the inverse of the cache if it was already calculated and 
+## the matrix has not changed
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getmean()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setmean(inv)
+  inv
 }
